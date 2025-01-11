9529d92a3cc069836fa88f797dfc88a7908fc09d
Stefan Meissner <stefan.meissner@vista.com>
2025-01-11 20:04:16 +0100

    fonts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-docker run --rm -i -v $(pwd):/workdir texlive/texlive latexmk -pdf -jobname=main <<EOF
+docker run --rm -i -v $(pwd):/workdir texlive/texlive latexmk -pdflua -jobname=main <<EOF
     \newcommand\type{XJDF}
     \newcommand\version{1.9}
     \newcommand\gitsha{xxx}
