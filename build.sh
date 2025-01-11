3e05b29f1e1e9800ecf9797f2dceb1b577c04a15
Stefan Meissner <stefan.meissner@vista.com>
2025-01-11 20:33:25 +0100

    ci optimizations

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
-docker run --rm -i -v $(pwd):/workdir texlive/texlive latexmk -pdflua -jobname=main <<EOF
-    \newcommand\type{XJDF}
+
+cat <<EOF > build.tex
+    \newcommand\type{JDF}
     \newcommand\version{1.9}
-    \newcommand\gitsha{xxx}
+    \newcommand\gitsha{abc123xyz}
     \input{main.tex}
 EOF
+
+docker run --rm -i -v $(pwd):/workdir texlive/texlive latexmk -pdflua -jobname=main build.tex
+
+rm build.tex
